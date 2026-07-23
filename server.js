ff1b1e92c139b4b140af6bcefa9f507181932710
RauchR90 <rauch33@gmail.com>
2026-07-23 17:13:31 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -14,14 +14,117 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'szerencsekerek.html'));
 });
 
-const rooms = {};
-const kerekMezok = [1000, 1000, 2000, 2000, 4000, 4000, 5000, 5000, 6000, 8000, 10000, 15000, 20000, "CSŐD", "PASSZ"];
 const alapFeladvanyok = [
+    // Magyar költők, írók (10)
     { kategoria: "Magyar költő, író", szoveg: "PETŐFI SÁNDOR" }, { kategoria: "Magyar költő, író", szoveg: "ARANY JÁNOS" },
-    { kategoria: "Magyar település", szoveg: "SIÓFOK" }, { kategoria: "Magyar település", szoveg: "DEBRECEN" },
+    { kategoria: "Magyar költő, író", szoveg: "RADNÓTI MIKLÓS" }, { kategoria: "Magyar költő, író", szoveg: "MÓRICZ ZSIGMOND" },
+    { kategoria: "Magyar költő, író", szoveg: "ADY ENDRE" }, { kategoria: "Magyar költő, író", szoveg: "JÓZSEF ATTILA" },
+    { kategoria: "Magyar költő, író", szoveg: "KOSZTOLÁNYI DEZSŐ" }, { kategoria: "Magyar költő, író", szoveg: "BABITS MIHÁLY" },
+    { kategoria: "Magyar költő, író", szoveg: "GÁRDONYI GÉZA" }, { kategoria: "Magyar költő, író", szoveg: "SZABÓ MAGDA" },
+
+    // Magyar települések (35)
+    { kategoria: "Magyar település", szoveg: "SIÓFOK" }, { kategoria: "Magyar település", szoveg: "SZENTENDRE" },
+    { kategoria: "Magyar település", szoveg: "VISEGRÁD" }, { kategoria: "Magyar település", szoveg: "HAJDÚSZOBOSZLÓ" },
+    { kategoria: "Magyar település", szoveg: "KARCAG" }, { kategoria: "Magyar település", szoveg: "ESZTERGOM" },
+    { kategoria: "Magyar település", szoveg: "TAPOLCA" }, { kategoria: "Magyar település", szoveg: "SOPRON" },
+    { kategoria: "Magyar település", szoveg: "KESZTHELY" }, { kategoria: "Magyar település", szoveg: "BALATONFÜRED" },
+    { kategoria: "Magyar település", szoveg: "HÓDMEZŐVÁSÁRHELY" }, { kategoria: "Magyar település", szoveg: "ZALAEGERSZEG" },
+    { kategoria: "Magyar település", szoveg: "BÉKÉSCSABA" }, { kategoria: "Magyar település", szoveg: "GYULA" },
+    { kategoria: "Magyar település", szoveg: "TIHANY" }, { kategoria: "Magyar település", szoveg: "DEBRECEN" },
+    { kategoria: "Magyar település", szoveg: "MISKOLC" }, { kategoria: "Magyar település", szoveg: "SZEGED" },
+    { kategoria: "Magyar település", szoveg: "PÉCS" }, { kategoria: "Magyar település", szoveg: "GYŐR" },
+    { kategoria: "Magyar település", szoveg: "SZÉKESFEHÉRVÁR" }, { kategoria: "Magyar település", szoveg: "SZOMBATHELY" },
+    { kategoria: "Magyar település", szoveg: "SZOLNOK" }, { kategoria: "Magyar település", szoveg: "TATABÁNYA" },
+    { kategoria: "Magyar település", szoveg: "KAPOSVÁR" }, { kategoria: "Magyar település", szoveg: "ÉRD" },
+    { kategoria: "Magyar település", szoveg: "DUNAKESZI" }, { kategoria: "Magyar település", szoveg: "CEGLÉD" },
+    { kategoria: "Magyar település", szoveg: "BAJA" }, { kategoria: "Magyar település", szoveg: "VÁC" },
+    { kategoria: "Magyar település", szoveg: "GÖDÖLLŐ" }, { kategoria: "Magyar település", szoveg: "KISKUNFÉLEGYHÁZA" },
+    { kategoria: "Magyar település", szoveg: "SZENTES" }, { kategoria: "Magyar település", szoveg: "GYÖNGYÖS" },
+    { kategoria: "Magyar település", szoveg: "KISVÁRDA" },
+
+    // Külföldi város vagy ország (38)
+    { kategoria: "Külföldi város vagy ország", szoveg: "OLASZORSZÁG" }, { kategoria: "Külföldi város vagy ország", szoveg: "AMSTERDAM" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "MADRID" }, { kategoria: "Külföldi város vagy ország", szoveg: "JAPÁN" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "BARCELONA" }, { kategoria: "Külföldi város vagy ország", szoveg: "PÁRIZS" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "LONDON" }, { kategoria: "Külföldi város vagy ország", szoveg: "SVÉDORSZÁG" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "FINNORSZÁG" }, { kategoria: "Külföldi város vagy ország", szoveg: "RÓMA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "AUSZTRÁLIA" }, { kategoria: "Külföldi város vagy ország", szoveg: "KANADA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "ARGENTÍNA" }, { kategoria: "Külföldi város vagy ország", szoveg: "TOKIÓ" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "BERLIN" }, { kategoria: "Külföldi város vagy ország", szoveg: "BÉCS" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "PRÁGA" }, { kategoria: "Külföldi város vagy ország", szoveg: "AMERIKAI EGYESÜLT ÁLLAMOK" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "SPANYOLORSZÁG" }, { kategoria: "Külföldi város vagy ország", szoveg: "FRANCIAORSZÁG" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "GÖRÖGORSZÁG" }, { kategoria: "Külföldi város vagy ország", szoveg: "TÖRÖKORSZÁG" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "EGYIPTOM" }, { kategoria: "Külföldi város vagy ország", szoveg: "KÍNA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "INDIA" }, { kategoria: "Külföldi város vagy ország", szoveg: "BRAZÍLIA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "MEXIKÓ" }, { kategoria: "Külföldi város vagy ország", szoveg: "NEW YORK" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "LOS ANGELES" }, { kategoria: "Külföldi város vagy ország", szoveg: "MOSZKVA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "PEKING" }, { kategoria: "Külföldi város vagy ország", szoveg: "DUBAI" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "ATHÉN" }, { kategoria: "Külföldi város vagy ország", szoveg: "LISSZABON" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "BOGOTA" }, { kategoria: "Külföldi város vagy ország", szoveg: "LIMA" },
+    { kategoria: "Külföldi város vagy ország", szoveg: "KAIRÓ" }, { kategoria: "Külföldi város vagy ország", szoveg: "SYDNEY" },
+
+    // Étel és ital (15)
     { kategoria: "Étel és ital", szoveg: "GULYÁSLEVES" }, { kategoria: "Étel és ital", szoveg: "SOMLÓI GALUSKA" },
-    { kategoria: "Film vagy sorozat", szoveg: "STAR WARS" }, { kategoria: "Film vagy sorozat", szoveg: "GLADIÁTOR" },
-    { kategoria: "Használati tárgy", szoveg: "MIKROHULLÁMÚ SÜTŐ" }, { kategoria: "Használati tárgy", szoveg: "PORSZÍVÓ" }
+    { kategoria: "Étel és ital", szoveg: "CAPPUCCINO" }, { kategoria: "Étel és ital", szoveg: "MARLENKA" },
+    { kategoria: "Étel és ital", szoveg: "PALACSINTA" }, { kategoria: "Étel és ital", szoveg: "TÖLTÖTT KÁPOSZTA" },
+    { kategoria: "Étel és ital", szoveg: "RÁNTOTT SAJT" }, { kategoria: "Étel és ital", szoveg: "GYÜMÖLCSLÉ" },
+    { kategoria: "Étel és ital", szoveg: "FORRÓ CSOKOLÁDÉ" }, { kategoria: "Étel és ital", szoveg: "MEGGYLEVES" },
+    { kategoria: "Étel és ital", szoveg: "TÚRÓS CSUSZA" }, { kategoria: "Étel és ital", szoveg: "MADÁRTEJ" },
+    { kategoria: "Étel és ital", szoveg: "KAKAÓS CSIGA" }, { kategoria: "Étel és ital", szoveg: "LIMONÁDÉ" },
+    { kategoria: "Étel és ital", szoveg: "HAMBURGER" },
+
+    // Állatvilág (15)
+    { kategoria: "Állatvilág", szoveg: "KANÁRI" }, { kategoria: "Állatvilág", szoveg: "CSÁSZÁRPINGVIN" },
+    { kategoria: "Állatvilág", szoveg: "ZSIRÁF" }, { kategoria: "Állatvilág", szoveg: "ELEFÁNT" },
+    { kategoria: "Állatvilág", szoveg: "OROSZLÁN" }, { kategoria: "Állatvilág", szoveg: "VÍZILÓ" },
+    { kategoria: "Állatvilág", szoveg: "JEGESMEDVE" }, { kategoria: "Állatvilág", szoveg: "GORILLA" },
+    { kategoria: "Állatvilág", szoveg: "KROKODIL" }, { kategoria: "Állatvilág", szoveg: "DELFIN" },
+    { kategoria: "Állatvilág", szoveg: "KENGURU" }, { kategoria: "Állatvilág", szoveg: "VADKAN" },
+    { kategoria: "Állatvilág", szoveg: "SZARVAS" }, { kategoria: "Állatvilág", szoveg: "MÓKUS" },
+    { kategoria: "Állatvilág", szoveg: "BORZ" },
+
+    // Film és sorozat (15)
+    { kategoria: "Film vagy sorozat", szoveg: "GLADIÁTOR" }, { kategoria: "Film vagy sorozat", szoveg: "STAR WARS" },
+    { kategoria: "Film vagy sorozat", szoveg: "JURASSIC PARK" }, { kategoria: "Film vagy sorozat", szoveg: "INCEPTION" },
+    { kategoria: "Film vagy sorozat", szoveg: "SHREK" }, { kategoria: "Film vagy sorozat", szoveg: "TITANIC" },
+    { kategoria: "Film vagy sorozat", szoveg: "AVATAR" }, { kategoria: "Film vagy sorozat", szoveg: "HARRY POTTER" },
+    { kategoria: "Film vagy sorozat", szoveg: "MÁTRIX" }, { kategoria: "Film vagy sorozat", szoveg: "A GYŰRŰK URA" },
+    { kategoria: "Film vagy sorozat", szoveg: "RESZKESSETEK BETÖRŐK" }, { kategoria: "Film vagy sorozat", szoveg: "JÓBARÁTOK" },
+    { kategoria: "Film vagy sorozat", szoveg: "TRÓNOK HARCA" }, { kategoria: "Film vagy sorozat", szoveg: "AGYAMENŐK" },
+    { kategoria: "Film vagy sorozat", szoveg: "BREAKING BAD" },
+
+    // Használati tárgy (61)
+    { kategoria: "Használati tárgy", szoveg: "MIKROHULLÁMÚ SÜTŐ" }, { kategoria: "Használati tárgy", szoveg: "VEZETÉK NÉLKÜLI EGÉR" },
+    { kategoria: "Használati tárgy", szoveg: "KÁVÉFŐZŐ" }, { kategoria: "Használati tárgy", szoveg: "PORSZÍVÓ" },
+    { kategoria: "Használati tárgy", szoveg: "MOSÓGÉP" }, { kategoria: "Használati tárgy", szoveg: "HAJSZÁRÍTÓ" },
+    { kategoria: "Használati tárgy", szoveg: "HŰTŐSZEKRÉNY" }, { kategoria: "Használati tárgy", szoveg: "VASALÓ" },
+    { kategoria: "Használati tárgy", szoveg: "MOSOGATÓGÉP" }, { kategoria: "Használati tárgy", szoveg: "FŰNYÍRÓ" },
+    { kategoria: "Használati tárgy", szoveg: "OKOSTELEFON" }, { kategoria: "Használati tárgy", szoveg: "TABLET" },
+    { kategoria: "Használati tárgy", szoveg: "BILLENTYŰZET" }, { kategoria: "Használati tárgy", szoveg: "FÜLHALLGATÓ" },
+    { kategoria: "Használati tárgy", szoveg: "ASZTALI LÁMPA" }, { kategoria: "Használati tárgy", szoveg: "ÉBRESZTŐÓRA" },
+    { kategoria: "Használati tárgy", szoveg: "FALIÓRA" }, { kategoria: "Használati tárgy", szoveg: "TÁVIRÁNYÍTÓ" },
+    { kategoria: "Használati tárgy", szoveg: "KULCSTARTÓ" }, { kategoria: "Használati tárgy", szoveg: "NAPSZEMÜVEG" },
+    { kategoria: "Használati tárgy", szoveg: "ESERNYŐ" }, { kategoria: "Használati tárgy", szoveg: "HÁTIZSÁK" },
+    { kategoria: "Használati tárgy", szoveg: "PÉNZTÁRCA" }, { kategoria: "Használati tárgy", szoveg: "CSAVARHÚZÓ" },
+    { kategoria: "Használati tárgy", szoveg: "KALAPÁCS" }, { kategoria: "Használati tárgy", szoveg: "FOGKEFE" },
+    { kategoria: "Használati tárgy", szoveg: "TÜKÖR" }, { kategoria: "Használati tárgy", szoveg: "FÉSŰ" },
+    { kategoria: "Használati tárgy", szoveg: "TÖRÖLKÖZŐ" }, { kategoria: "Használati tárgy", szoveg: "SZAPPANADAGOLÓ" },
+    { kategoria: "Használati tárgy", szoveg: "KANAPÉ" }, { kategoria: "Használati tárgy", szoveg: "FOTEL" },
+    { kategoria: "Használati tárgy", szoveg: "RUHASSZEKRÉNY" }, { kategoria: "Használati tárgy", szoveg: "KÉPKERET" },
+    { kategoria: "Használati tárgy", szoveg: "KÖNYVESPOLC" }, { kategoria: "Használati tárgy", szoveg: "VIRÁGCSERÉP" },
+    { kategoria: "Használati tárgy", szoveg: "PÁRNA" }, { kategoria: "Használati tárgy", szoveg: "TAKARÓ" },
+    { kategoria: "Használati tárgy", szoveg: "LEPEDŐ" }, { kategoria: "Használati tárgy", szoveg: "OLLÓ" },
+    { kategoria: "Használati tárgy", szoveg: "RAGASZTÓSZALAG" }, { kategoria: "Használati tárgy", szoveg: "TOLLTARTÓ" },
+    { kategoria: "Használati tárgy", szoveg: "CSISZOLÓPAPÍR" }, { kategoria: "Használati tárgy", szoveg: "LÉTRA" },
+    { kategoria: "Használati tárgy", szoveg: "POHÁR" }, { kategoria: "Használati tárgy", szoveg: "TÁNYÉR" },
+    { kategoria: "Használati tárgy", szoveg: "EVŐESZKÖZ" }, { kategoria: "Használati tárgy", szoveg: "KANÁL" },
+    { kategoria: "Használati tárgy", szoveg: "VILLA" }, { kategoria: "Használati tárgy", szoveg: "KÉS" },
+    { kategoria: "Használati tárgy", szoveg: "VÁGÓDESZKA" }, { kategoria: "Használati tárgy", szoveg: "MERŐKANÁL" },
+    { kategoria: "Használati tárgy", szoveg: "HABVERŐ" }, { kategoria: "Használati tárgy", szoveg: "SERPENYŐ" },
+    { kategoria: "Használati tárgy", szoveg: "FAZÉK" }, { kategoria: "Használati tárgy", szoveg: "TEPSI" },
+    { kategoria: "Használati tárgy", szoveg: "KUKTA" }, { kategoria: "Használati tárgy", szoveg: "RESZELŐ" },
+    { kategoria: "Használati tárgy", szoveg: "DUGÓHÚZÓ" }, { kategoria: "Használati tárgy", szoveg: "KONZERVNYITÓ" },
+    { kategoria: "Használati tárgy", szoveg: "HÚSCLOPFOLÓ" }
 ];
 
 function getRoom(roomName) {
